e2e6485365d0fda06dea32b0066a85cc1b93bbb5
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-06-22 20:22:02 +0900

    Create_SignUp_LogIn_Views

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
-# from users.views import
+from users.views import SighUpView, LogInView
 
 urlpatterns = [
+    path('/signup', SighUpView.as_view()),
+    path('/login', LogInView.as_view())
 ]
