d4328c7ee4648acea06ddac84f504836400a9793
wsvgc95 <wsvgc95@gim-usig-ui-MacBookAir.local>
2022-06-30 15:50:11 +0900

    UPDATE: Create_NavView

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from products.views import ProductListView, CartView, SizeView, ProductView, CategoryView
+from products.views import ProductListView, CartView, SizeView, ProductView, CategoryView, NavView, UserNavView
 
 urlpatterns = [
     path('/<int:product_id>', ProductView.as_view()),
     path('/sizes', SizeView.as_view()),
     path('/carts', CartView.as_view()),
-    path('/lists', ProductListView.as_view()),
-    path('/categories', CategoryView.as_view())
+    path('', ProductListView.as_view()),
+    path('/categories', CategoryView.as_view()),
+    path('/nav', NavView.as_view()),
+    path('/user_nav', UserNavView.as_view())
 ]
